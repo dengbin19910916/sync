77e71a618f0bff3ec04d2268e7e183e6fc75632d
Dengbin <dengbin@winnermedical.com>
2020-08-23 22:34:24 +0800

    clean code

--- src/main/kotlin/io/xxx/sync/core/ScheduleCalendar.kt
+++ src/main/kotlin/io/xxx/sync/core/ScheduleCalendar.kt
@@ -32,14 +32,11 @@ class ScheduleCalendar : Job {
         wrapper.last("limit 1")
         val lastSchedule = scheduleMapper.selectOne(wrapper)
         val startTime = lastSchedule?.endTime ?: property.originTime
-        val now = LocalDateTime.now()
-        val maxEndTime = now.minusSeconds(property.delay.toLong())
-        val duration = Duration.between(startTime, maxEndTime)
-        val betweenSeconds = duration.seconds
-        val count = (betweenSeconds / property.timeInterval)
+        val maxEndTime = LocalDateTime.now().minusSeconds(property.delay.toLong())
+        val betweenSeconds = Duration.between(startTime, maxEndTime).seconds
 
         val schedules = mutableListOf<SyncSchedule>()
-        for (i in 0 until count) {
+        for (i in 0 until betweenSeconds / property.timeInterval) {
             val newStartTime = startTime.plusSeconds(property.timeInterval * i)
             val schedule = buildSchedule(property, newStartTime)
             schedules.add(schedule)
